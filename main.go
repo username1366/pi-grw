2e620a4caacc3331f4d45c2b090e881d3393c8d4
alexander.kosenko <alexander.kosenko@loopme.com>
2018-03-18 01:50:32 +0200

    Added more gpio ports

--- main.go
+++ main.go
@@ -7,15 +7,35 @@ import (
 )
 
 func main() {
-	p24, err := rpi.OpenPin(24, rpi.OUT)
-	if err != nil {
+	p11, _ := rpi.OpenPin(11, rpi.OUT)
+	p13, _ := rpi.OpenPin(13, rpi.OUT)
+	p15, _ := rpi.OpenPin(15, rpi.OUT)
+	p18, _ := rpi.OpenPin(18, rpi.OUT)
+	p23, _ := rpi.OpenPin(23, rpi.OUT)
+	p24, _ := rpi.OpenPin(24, rpi.OUT)
+	/*if err != nil {
 		panic(err)
-	}
+	}*/
+	defer p11.Close()
+	defer p13.Close()
+	defer p15.Close()
+	defer p18.Close()
+	defer p23.Close()
 	defer p24.Close()
 
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 5; i++ {
+		p11.Write(rpi.HIGH)
+		p13.Write(rpi.HIGH)
+		p15.Write(rpi.HIGH)
+		p18.Write(rpi.HIGH)
+		p23.Write(rpi.HIGH)
 		p24.Write(rpi.HIGH)
 		time.Sleep(300 * time.Millisecond)
+		p11.Write(rpi.LOW)
+		p13.Write(rpi.LOW)
+		p15.Write(rpi.LOW)
+		p18.Write(rpi.LOW)
+		p23.Write(rpi.LOW)
 		p24.Write(rpi.LOW)
 		time.Sleep(100 * time.Millisecond)
 	}
