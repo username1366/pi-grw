85deb64ac61ed3578503c61c48a5346c401b326c
alexander.kosenko <alexander.kosenko@loopme.com>
2018-03-18 01:17:23 +0200

    Changed gpio package

--- main.go
+++ main.go
@@ -5,29 +5,47 @@ import (
 
 	"time"
 
-	"github.com/kidoman/embd"
-	_ "github.com/kidoman/embd/host/all"
+	rpio "github.com/stianeikeland/go-rpio"
+	//	"github.com/kidoman/embd"
+	//	_ "github.com/kidoman/embd/host/all"
 )
 
 func main() {
 	str := "Hello"
 	fmt.Printf("%s\n", str)
+	_ = rpio.Open()
+	pin11 := rpio.Pin(11)
 
-	embd.InitGPIO()
-	defer embd.CloseGPIO()
-	embd.SetDirection(11, embd.Out)
-	embd.DigitalWrite(11, embd.High)
-	embd.SetDirection(13, embd.Out)
-	embd.DigitalWrite(13, embd.High)
-	embd.SetDirection(15, embd.Out)
-	embd.DigitalWrite(15, embd.High)
-	time.Sleep(3 * time.Second)
+	pin11.Output() // Output mode
+	pin11.High()   // Set pin High
+	pin11.Low()    // Set pin Low
+	pin11.Toggle() // Toggle pin (Low -> High -> Low)
 
-	embd.SetDirection(11, embd.Out)
-	embd.DigitalWrite(11, embd.Low)
-	embd.SetDirection(13, embd.Out)
-	embd.DigitalWrite(13, embd.Low)
-	embd.SetDirection(15, embd.Out)
-	embd.DigitalWrite(15, embd.Low)
+	time.Sleep(2 * time.Second)
 
+	pin18 := rpio.Pin(18)
+
+	pin18.Output() // Output mode
+	pin18.High()   // Set pin High
+	pin18.Low()    // Set pin Low
+	pin18.Toggle() // Toggle pin (Low -> High -> Low)
+
+	/*
+		embd.InitGPIO()
+		defer embd.CloseGPIO()
+		embd.SetDirection(11, embd.Out)
+		embd.DigitalWrite(11, embd.High)
+		embd.SetDirection(13, embd.Out)
+		embd.DigitalWrite(13, embd.High)
+		embd.SetDirection(15, embd.Out)
+		embd.DigitalWrite(15, embd.High)
+		time.Sleep(3 * time.Second)
+
+		embd.SetDirection(11, embd.Out)
+		embd.DigitalWrite(11, embd.Low)
+		embd.SetDirection(13, embd.Out)
+		embd.DigitalWrite(13, embd.Low)
+		embd.SetDirection(15, embd.Out)
+		embd.DigitalWrite(15, embd.Low)
+	*/
 }
